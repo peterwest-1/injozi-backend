d7e23cc52a0a5cdc8d1da71a9986053bb2dadf63
Pete West <10847430+peterwest-1@users.noreply.github.com>
2023-03-29 16:27:35 +0200

    Refactored models

--- injozi/models.py
+++ injozi/models.py
@@ -1,5 +1,12 @@
 import datetime
-from flask_mongoengine import MongoEngine as db
+from injozi import db
+
+
+class User(db.Document):
+    email = db.StringField(required=True, unique=True)
+    password = db.StringField(required=True, min_length=6)
+    created = db.DateTimeField(default=datetime.datetime.now())
+    updated = db.DateTimeField(default=datetime.datetime.now())
 
 
 class Profile(db.Document):
--- injozi/models/User.py
+++
@@ -1,9 +0,0 @@
-import datetime
-from flask_mongoengine import MongoEngine as db
-
-
-class User(db.Document):
-    email = db.StringField(required=True, unique=True)
-    password = db.StringField(required=True, min_length=6)
-    created = db.DateTimeField(default=datetime.datetime.now())
-    updated = db.DateTimeField(default=datetime.datetime.now())
